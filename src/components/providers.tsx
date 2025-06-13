f8bdb2217afc5771339d7ae0bff5131c44535125
Alan Shen <alan.shen27@gmail.com>
2025-06-13 20:16:31 +0200

    oksdafpsdf

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,6 +3,6 @@
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-export function Providers({ children, session }: { children: React.ReactNode; session: Session }) {
+export function Providers({ children, session }: { children: React.ReactNode; session: Session | null }) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
--- src/types/auth.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@auth/core/adapters" {
-  export type JsonObject = {
-    [Key in string]?: JsonValue;
-  };
-  export type JsonArray = JsonValue[];
-  export type JsonValue = string | number | boolean | null | JsonObject | JsonArray;
-} 
